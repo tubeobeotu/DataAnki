b0a1f79ccac193e1acb4b705d994c6fb208f9162
Tu <example@email.com>
2019-04-03 22:09:58 +0700

    update

---
+++ CardhopTNV/CardhopTNV/Cells/BaseTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  BaseTableViewCell.swift
+//  CardhopTNV
+//
+//  Created by Tu on 4/3/19.
+//  Copyright © 2019 tunv. All rights reserved.
+//
+
+import UIKit
+
+class BaseTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    class func nidName() -> String{
+        return ""
+    }
+    
+    class func identifierName() -> String{
+        return ""
+    }
+
+}
---
+++ CardhopTNV/CardhopTNV/Cells/ContactCell.swift
@@ -0,0 +1,24 @@
+//
+//  ContactCell.swift
+//  CardhopTNV
+//
+//  Created by Tu on 4/3/19.
+//  Copyright © 2019 tunv. All rights reserved.
+//
+
+import UIKit
+
+class ContactCell: BaseTableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
