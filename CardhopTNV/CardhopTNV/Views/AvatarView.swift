f3693b8bfb7f4ecff1da066203df852a4c249931
Tu <example@email.com>
2019-04-05 08:06:48 +0700

    update

---
+++ CardhopTNV/CardhopTNV/Views/AvatarView.swift
@@ -0,0 +1,18 @@
+//
+//  AvatarView.swift
+//  CardhopTNV
+//
+//  Created by Tu on 4/5/19.
+//  Copyright © 2019 tunv. All rights reserved.
+//
+
+import UIKit
+
+class AvatarView: BaseCustomNibView {
+
+    @IBOutlet weak var img_Profile: UIImageView!
+    
+    @IBOutlet weak var v_Name: UIView!
+    @IBOutlet weak var lbl_Name: UILabel!
+    
+}
---
+++ CardhopTNV/CardhopTNV/Views/BaseCustomNibView.swift
@@ -0,0 +1,43 @@
+import Foundation
+import UIKit
+class BaseCustomNibView: UIView
+{
+    var contentView: UIView!
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        nibSetup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        nibSetup()
+    }
+    private func nibSetup() {
+        backgroundColor = .clear
+        
+        contentView = loadViewFromNib()
+        contentView.frame = bounds
+        contentView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        contentView.translatesAutoresizingMaskIntoConstraints = true
+        
+        addSubview(contentView)
+        
+        setupViews()
+    }
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupViews()
+    }
+    func setupViews()
+    {
+        
+    }
+    
+    func loadViewFromNib() -> UIView {
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: String(describing: type(of: self)), bundle: bundle)
+        let nibView = nib.instantiate(withOwner: self, options: nil).first as! UIView
+        
+        return nibView
+    }
+}
