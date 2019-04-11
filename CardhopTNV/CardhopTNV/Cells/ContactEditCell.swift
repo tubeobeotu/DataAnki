c4e7df051660adac812cf3cc79c0b1adcb66097c
Tu <example@email.com>
2019-04-11 08:19:50 +0700

    update

---
+++ CardhopTNV/CardhopTNV/Cells/ContactEditCell.swift
@@ -0,0 +1,24 @@
+//
+//  ContactEditCell.swift
+//  CardhopTNV
+//
+//  Created by Tu on 4/11/19.
+//  Copyright © 2019 tunv. All rights reserved.
+//
+
+import UIKit
+
+class ContactEditCell: BaseTableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ CardhopTNV/CardhopTNV/Cells/ContactNewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ContactNewCell.swift
+//  CardhopTNV
+//
+//  Created by Tu on 4/11/19.
+//  Copyright © 2019 tunv. All rights reserved.
+//
+
+import UIKit
+
+class ContactNewCell: BaseTableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
